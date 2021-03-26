595a2ce9787b2743ae58a79b3237cf2cc5b99837
Alex Eckhart <eckhartalex@gmail.com>
2021-03-25 23:51:53 -0600

    Convert class components to hooked functional components.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { getStartState, State } from './logic/state'
 import { Move, Promotion } from './logic/moves'
 import * as immutable from 'immutable'
 import { Human, MrRandom, Player } from './player'
+import { useEffect, useReducer, useState } from 'react'
 
 interface BoardSquareProps {
   isBlack?: boolean
@@ -122,130 +123,117 @@ function PromoteMenu (props: PromoteMenuProps) {
   )
 }
 
-interface ChessBoardState {
-  highlightedPos: Pos | null
-  promotePos: Pos | null
-}
-
 interface ChessBoardProps {
   state: State
   makeMove?: (state: State) => void
 }
 
-class ChessBoard extends React.PureComponent<ChessBoardProps, ChessBoardState> {
-  constructor (props: ChessBoardProps) {
-    super(props)
-    this.state = {
-      highlightedPos: null,
-      promotePos: null
-    }
+function ChessBoard (props: ChessBoardProps) {
+  const [highlightedPos, setHighlightedPos] = useState(null as Pos | null)
+  const [promotePos, setPromotePos] = useState(null as Pos | null)
+
+  if (highlightedPos && !props.state.board.get(highlightedPos).isOccupied()) {
+    setHighlightedPos(null)
   }
 
-  static getDerivedStateFromProps (props: ChessBoardProps, state: ChessBoardState) {
-    if (state.highlightedPos && !props.state.board.get(state.highlightedPos).isOccupied()) {
-      return { highlightedPos: null }
-    }
-    return null
+  const squares = []
+  let isBlack = false
+  let moves: Move[] = []
+  let drawPromotePos = promotePos
+  if (drawPromotePos && props.state.currTurn === BLACK) {
+    drawPromotePos = drawPromotePos.addRank(3)
   }
-
-  render (): React.ReactElement {
-    const squares = []
-    let isBlack = false
-    let moves: Move[] = []
-    const highlightedPos = this.state.highlightedPos
-    let drawPromotePos = this.state.promotePos
-    if (drawPromotePos && this.props.state.currTurn === BLACK) {
-      drawPromotePos = drawPromotePos.addRank(3)
-    }
-    if (highlightedPos && this.props.makeMove) {
-      moves = this.props.state.moves().filter(move => {
-        if (move.invalid() || this.props.state.isGameOver()) {
-          return false
-        }
+  if (highlightedPos && props.makeMove) {
+    moves = props.state.moves().filter(move => {
+      if (move.invalid() || props.state.isGameOver()) {
+        return false
+      }
+      if (move.isNormal()) {
+        return move.fromPos.compare(highlightedPos) === 0
+      }
+      if (move.isCastle()) {
+        return highlightedPos.rank === props.state.currTurn.KING_RANK && highlightedPos.file === 4
+      }
+      return false
+    })
+  }
+  for (let i = 7; i >= 0; i--) {
+    isBlack = !isBlack
+    for (let j = 0; j < 8; j++) {
+      isBlack = !isBlack
+      const pos = new Pos(j, i)
+      const highlighted = Boolean(highlightedPos && pos.compare(highlightedPos) === 0)
+      const piece = props.state.board.get(new Pos(j, i))
+      const inCheck = piece === props.state.currTurn.KING && props.state.isCheck()
+      const moveIndex = moves.findIndex(move => {
         if (move.isNormal()) {
-          return move.fromPos.compare(highlightedPos) === 0
+          return move.toPos.compare(pos) === 0
         }
         if (move.isCastle()) {
-          return highlightedPos.rank === this.props.state.currTurn.KING_RANK && highlightedPos.file === 4
+          if (pos.rank !== props.state.currTurn.KING_RANK) {
+            return false
+          }
+          if (move.isKingSide) {
+            return pos.file === 7 || pos.file === 6
+          } else {
+            return pos.file === 2 || pos.file === 0
+          }
         }
         return false
       })
-    }
-    for (let i = 7; i >= 0; i--) {
-      isBlack = !isBlack
-      for (let j = 0; j < 8; j++) {
-        isBlack = !isBlack
-        const pos = new Pos(j, i)
-        const highlighted = Boolean(highlightedPos && pos.compare(highlightedPos) === 0)
-        const piece = this.props.state.board.get(new Pos(j, i))
-        const inCheck = piece === this.props.state.currTurn.KING && this.props.state.isCheck()
-        const moveIndex = moves.findIndex(move => {
-          if (move.isNormal()) {
-            return move.toPos.compare(pos) === 0
-          }
-          if (move.isCastle()) {
-            if (pos.rank !== this.props.state.currTurn.KING_RANK) {
-              return false
-            }
-            if (move.isKingSide) {
-              return pos.file === 7 || pos.file === 6
-            } else {
-              return pos.file === 2 || pos.file === 0
-            }
-          }
-          return false
-        })
-        let onClick = () => {
-          this.setState({ highlightedPos: null, promotePos: null })
-        }
-        const move = moves[moveIndex]
-        if (moveIndex === -1 && piece.isOccupied()) {
-          onClick = () => {
-            this.setState({ highlightedPos: pos, promotePos: null })
-          }
-        } else if (moveIndex >= 0 && ((move.isNormal() && !move.isPromote()) || move.isCastle())) {
-          onClick = () => {
-            this.props.makeMove!(moves[moveIndex].do())
-            this.setState({ highlightedPos: null, promotePos: null })
-          }
-        } else if (moveIndex >= 0 && move.isNormal() && move.isPromote() && !this.state.promotePos) {
-          const promote = move
-          onClick = () => {
-            this.setState({ promotePos: promote.toPos })
-          }
-        }
-        let promoteMenu
-        if (drawPromotePos && pos.compare(drawPromotePos) === 0) {
-          const onPromote = (state: State) => {
-            this.props.makeMove!(state)
-            this.setState({ highlightedPos: null, promotePos: null })
-          }
-          const that = this
-          function PromoteFilter (move: Move): move is Promotion {
-            if (!move.isNormal() || !that.state.promotePos || move.toPos.compare(that.state.promotePos) !== 0) {
-              return false
-            }
-            return move.isPromote()
-          }
-
-          const promoteList = immutable.List(moves.filter<Promotion>(PromoteFilter))
-
-          promoteMenu = <PromoteMenu moves={promoteList} onPromote={onPromote} />
-        }
-        const canMoveTo = moveIndex >= 0 && !this.state.promotePos
-        squares.push(
-            <BoardSquare key={i * 8 + j} canMoveTo={canMoveTo} isBlack={isBlack} piece={piece} highlighted={highlighted} inCheck={inCheck} onClick={onClick}>
-              {promoteMenu}
-            </BoardSquare>
-        )
+      let onClick = () => {
+        setHighlightedPos(null)
+        setPromotePos(null)
       }
+      const move = moves[moveIndex]
+      if (moveIndex === -1 && piece.isOccupied()) {
+        onClick = () => {
+          setHighlightedPos(pos)
+          setPromotePos(null)
+        }
+      } else if (moveIndex >= 0 && ((move.isNormal() && !move.isPromote()) || move.isCastle())) {
+        onClick = () => {
+          props.makeMove!(moves[moveIndex].do())
+          setHighlightedPos(null)
+          setPromotePos(null)
+        }
+      } else if (moveIndex >= 0 && move.isNormal() && move.isPromote() && !promotePos) {
+        const promote = move
+        onClick = () => {
+          setPromotePos(promote.toPos)
+        }
+      }
+      let promoteMenu
+      if (drawPromotePos && pos.compare(drawPromotePos) === 0) {
+        const onPromote = (state: State) => {
+          props.makeMove!(state)
+          setHighlightedPos(null)
+          setPromotePos(null)
+        }
+        function PromoteFilter (move: Move): move is Promotion {
+          if (!move.isNormal() || !promotePos || move.toPos.compare(promotePos) !== 0) {
+            return false
+          }
+          return move.isPromote()
+        }
+
+        const promoteList = immutable.List(moves.filter<Promotion>(PromoteFilter))
+
+        promoteMenu = <PromoteMenu moves={promoteList} onPromote={onPromote} />
+      }
+      const canMoveTo = moveIndex >= 0 && !promotePos
+      squares.push(
+        <BoardSquare key={i * 8 + j} canMoveTo={canMoveTo} isBlack={isBlack} piece={piece} highlighted={highlighted} inCheck={inCheck} onClick={onClick}>
+          {promoteMenu}
+        </BoardSquare>
+      )
     }
-    return (
-        <div className="ChessBoard">
-          {squares}
-        </div>
-    )
   }
+  return (
+    <div className="ChessBoard">
+      {squares}
+    </div>
+  )
 }
 
 type PlayerSel = 'human' | 'random'
@@ -254,22 +242,12 @@ interface PlayerSelectorProps {
   onPlayerChange: (playerCons: new () => Player) => void
 }
 
-interface PlayerSelectorState {
-  currSel: PlayerSel
-}
+function PlayerSelector (props: PlayerSelectorProps) {
+  const [currSel, setCurrSel] = useState('human' as PlayerSel)
 
-class PlayerSelector extends React.PureComponent<PlayerSelectorProps, PlayerSelectorState> {
-  constructor (props: PlayerSelectorProps) {
-    super(props)
-    this.state = {
-      currSel: 'human'
-    }
-    this.onChange = this.onChange.bind(this)
-  }
-
-  onChange (event: React.ChangeEvent<HTMLSelectElement>) {
+  const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const newSel = event.target.value as PlayerSel
-    this.setState({ currSel: newSel })
+    setCurrSel(newSel)
     let playerCons
     switch (newSel) {
       case 'human':
@@ -280,21 +258,19 @@ class PlayerSelector extends React.PureComponent<PlayerSelectorProps, PlayerSele
         break
     }
 
-    this.props.onPlayerChange(playerCons)
+    props.onPlayerChange(playerCons)
   }
 
-  render () {
-    return (
-      <select value={this.state.currSel} onChange={this.onChange}>
-        <option value='human'>
-          Human
-        </option>
-        <option value='random'>
-          MrRandom
-        </option>
-      </select>
-    )
-  }
+  return (
+    <select value={currSel} onChange={onChange}>
+      <option value='human'>
+        Human
+      </option>
+      <option value='random'>
+        MrRandom
+      </option>
+    </select>
+  )
 }
 
 interface GameInfoProps {
@@ -302,125 +278,92 @@ interface GameInfoProps {
   restart: (white: Player, black: Player, state: State) => void
 }
 
-interface GameInfoState {
-  WhiteCons: new () => Player,
-  BlackCons: new () => Player,
+function GameInfo (props: GameInfoProps) {
+  const [[WhiteCons], setWhiteCons] = useState([Human])
+  const [[BlackCons], setBlackCons] = useState([Human])
+
+  const restartButton = () => {
+    props.restart(new WhiteCons(), new BlackCons(), getStartState())
+  }
+
+  const onWhiteChange = (player: new () => Player) => {
+    setWhiteCons([player])
+  }
+
+  const onBlackChange = (player: new () => Player) => {
+    setBlackCons([player])
+  }
+
+  let checkmateText
+  if (props.state.isCheckmate()) {
+    checkmateText = 'Checkmate'
+  } else if (props.state.isDraw()) {
+    checkmateText = `Draw due to ${props.state.drawReason()}`
+  }
+  return (
+    <div className='GameInfo'>
+      {checkmateText}
+      <br/>
+      <button onClick={restartButton}>
+        Restart
+      </button>
+      <br/>
+      <label>White Player </label>
+      <PlayerSelector onPlayerChange={onWhiteChange} />
+      <br/>
+      <label>Black Player </label>
+      <PlayerSelector onPlayerChange={onBlackChange} />
+    </div>
+  )
 }
 
-class GameInfo extends React.Component<GameInfoProps, GameInfoState> {
-  constructor (props: GameInfoProps) {
-    super(props)
-    this.state = {
-      WhiteCons: Human,
-      BlackCons: Human
-    }
-    this.restartButton = this.restartButton.bind(this)
-    this.onWhiteChange = this.onWhiteChange.bind(this)
-    this.onBlackChange = this.onBlackChange.bind(this)
-  }
+function App (props: {}) {
+  const [white, setWhite] = useState(new Human())
+  const [black, setBlack] = useState(new Human())
+  const [state, setState] = useState(getStartState())
+  const forceUpdate = useReducer(x => x + 1, 0)[1]
 
-  onWhiteChange (player: new () => Player) {
-    this.setState({ WhiteCons: player })
-  }
-
-  onBlackChange (player: new () => Player) {
-    this.setState({ BlackCons: player })
-  }
-
-  restartButton () {
-    this.props.restart(new this.state.WhiteCons(), new this.state.BlackCons(), getStartState())
-  }
-
-  render (): React.ReactElement {
-    let checkmateText
-    if (this.props.state.isCheckmate()) {
-      checkmateText = 'Checkmate'
-    } else if (this.props.state.isDraw()) {
-      checkmateText = `Draw due to ${this.props.state.drawReason()}`
-    }
-    return (
-      <div className='GameInfo'>
-        {checkmateText}
-        <br/>
-        <button onClick={this.restartButton}>
-          Restart
-        </button>
-        <br/>
-        <label>White Player </label>
-        <PlayerSelector onPlayerChange={this.onWhiteChange} />
-        <br/>
-        <label>Black Player </label>
-        <PlayerSelector onPlayerChange={this.onBlackChange} />
-      </div>
-    )
-  }
-}
-
-interface AppState {
-  white: Player
-  black: Player
-  state: State
-}
-
-class App extends React.Component<{}, AppState> {
-  constructor (props: {}) {
-    super(props)
-    const white = new Human()
-    const black = new Human()
-    const state = getStartState()
-    this.state = {
-      state: state,
-      white,
-      black
-    }
-    this.restart = this.restart.bind(this)
-  }
-
-  restart (white: Player, black: Player, state: State) {
-    this.state.white.close()
-    this.state.black.close()
-    this.initPlayer(white, black, state)
-    this.setState({ white, black, state })
-  }
-
-  initPlayer (player1: Player, player2: Player, start: State) {
+  useEffect(() => {
+    let player1 = white
+    let player2 = black
     const makeMoveThen = (state: State): Promise<unknown> | undefined => {
       if (state.isGameOver()) {
-        this.setState({ state })
+        setState(state)
         return
       }
       const tmp = player1
       player1 = player2
       player2 = tmp
       const ret = player1.makeMove(state).then(makeMoveThen)
-      this.setState({ state })
+      setState(state)
       return ret
     }
-    player1.makeMove(start).then(makeMoveThen)
-  }
-
-  render () {
-    let currPlayer = this.state.white
-    if (this.state.state.currTurn === BLACK) {
-      currPlayer = this.state.black
+    player1.makeMove(getStartState()).then(makeMoveThen)
+    forceUpdate()
+    return () => {
+      white.close()
+      black.close()
     }
-    return (
-        <div className="App">
-          <ChessBoard makeMove={currPlayer.getBoardClick()} state={this.state.state} />
-          <GameInfo state={this.state.state} restart={this.restart} />
-        </div>
-    )
+  }, [white, black])
+
+  const restart = (white: Player, black: Player, state: State) => {
+    white.close()
+    black.close()
+    setWhite(white)
+    setBlack(black)
+    setState(state)
   }
 
-  componentDidMount () {
-    this.initPlayer(this.state.white, this.state.black, this.state.state)
-    this.forceUpdate()
+  let currPlayer = white
+  if (state.currTurn === BLACK) {
+    currPlayer = black
   }
-
-  componentWillUnmount () {
-    this.state.white.close()
-    this.state.black.close()
-  }
+  return (
+    <div className="App">
+      <ChessBoard makeMove={currPlayer.getBoardClick()} state={state} />
+      <GameInfo state={state} restart={restart} />
+    </div>
+  )
 }
 
 export default App
