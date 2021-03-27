ecaf1ba20896707f77c39193e374cb38bc376504
Alex Eckhart <eckhartalex@gmail.com>
2021-03-26 23:07:26 -0600

    Split app and playmode.

---
+++ src/PlayMode.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import { useEffect, useReducer, useState } from 'react'
+import { Human, Player } from './player'
+import { getStartState, State } from './logic/state'
+import { BLACK } from './logic/pieces'
+import ChessBoard from './ChessBoard'
+import GameInfo from './GameInfo'
+
+export default function PlayMode (props: {}) {
+  const [white, setWhite] = useState<Player>(new Human())
+  const [black, setBlack] = useState<Player>(new Human())
+  const beginState = getStartState()
+  const [state, setState] = useState(beginState)
+  const forceUpdate = useReducer(x => x + 1, 0)[1]
+
+  useEffect(() => {
+    let player1 = white
+    let player2 = black
+    const makeMoveThen = (state: State): Promise<unknown> | undefined => {
+      if (state.isGameOver()) {
+        setState(state)
+        return
+      }
+      const tmp = player1
+      player1 = player2
+      player2 = tmp
+      const ret = player1.makeMove(state).then(makeMoveThen)
+      setState(state)
+      return ret
+    }
+    player1.makeMove(beginState).then(makeMoveThen)
+    forceUpdate()
+    return () => {
+      white.close()
+      black.close()
+    }
+  }, [white, black])
+
+  const restart = (white: Player, black: Player) => {
+    white.close()
+    black.close()
+    setWhite(white)
+    setBlack(black)
+    setState(beginState)
+  }
+
+  let currPlayer = white
+  if (state.currTurn === BLACK) {
+    currPlayer = black
+  }
+  return (
+    <div className="App">
+      <ChessBoard makeMove={currPlayer.getBoardClick()} state={state}/>
+      <GameInfo state={state} restart={restart}/>
+    </div>
+  )
+}
